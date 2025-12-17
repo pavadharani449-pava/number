398163a2baec45a47795762cc41748ae54014752
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-17 09:56:48 +0530

    Add files via upload

---
+++ Lp3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Lp3 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner (System.in);
+          int n=sc.nextInt();
+        int j=0;
+        for(int i=1;i<=n;i++)
+        {
+            j++;
+        }
+        System.out.println(j);
+    }
+}
+
